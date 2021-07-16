29d47accc1cc06f3219245e1cd88c81a0ed5a06b
KhushiJain <khushi.jain@neosoftmail.com>
2021-07-15 17:55:48 -0700

    message changed in sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum{
 	public static void main(String[] a){
 		int num1=10, num2-20, sum;
 		sum=num1+num2;
-		System.out.println("Sum of these numbers: "+ sum);
+		System.out.println("Sum of " + num1 + " and " +  num2 + " is " + sum);
 	}
 }
 
