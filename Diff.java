2a510030702b7ace8b3b526c61c05cfd9ffe895e
KhushiJain <khushi.jain@neosoftmail.com>
2021-07-15 17:52:09 -0700

    added Diff.java

---
+++ Diff.java
@@ -0,0 +1,7 @@
+public class Sum{
+        public static void main(String[] a){
+                int num1=30, num2=20, sum;
+                sum=num1-num2;
+                System.out.println("Subtraction of " + num1 + " and " +  num2 + " is " + sum);
+        }
+}
